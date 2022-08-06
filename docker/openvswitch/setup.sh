ec3c1d544ae42107eb1cb113a5abd5d4e583aebd
mdewinged <alexandreamk1@gmail.com>
2022-08-06 09:01:54 -0300

    '.'

--- docker/openvswitch/setup.sh
+++ docker/openvswitch/setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-apt update
-RUNLEVEL=1 apt install -y --no-install-recommends openvswitch-switch sudo net-tools iproute2 iputils-ping nano iptables wget unzip libpcap-dev tcpdump sudo default-jdk
+#
+# Copyright (C) 2022 Alexandre Mitsuru Kaihara
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 # Add java to path
 echo -e "export JAVA_HOME=\"/usr/lib/jvm/java-1.11.0-openjdk-amd64\"\nexport PATH=$JAVA_HOME/bin:$PATH" >> /etc/profile
@@ -14,5 +29,8 @@ done
 rm master.zip
 
 mkdir /TCPDUMP_and_CICFlowMeter-master/collecteddata
+
+sed -i "s/trap/#trap/g" capture_interface_pcap.sh
+sed -i "s/trap/#trap/g" convert_pcap_csv.sh
 
 
